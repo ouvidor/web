08d749a4ccd28820a25939c56d157cab63c7c812
Pedro Pinho <rihor1000@gmail.com>
2019-08-24 23:30:58 -0300

    Add TagsList componente e stories

---
+++ src/components/TagsList/TagsList.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import TagsList from './index';
+
+const tags1 = [{ id: 1, title: 'Saúde' }];
+const tags2 = [{ id: 1, title: 'Saúde' }, { id: 2, title: 'Crime' }];
+const tags3 = [
+  { id: 1, title: 'Saúde' },
+  { id: 2, title: 'Crime' },
+  { id: 3, title: 'Eletricidade' },
+];
+
+storiesOf('TagsList', module)
+  .add('1 tag', () => <TagsList tags={tags1} />)
+  .add('2 tags', () => <TagsList tags={tags2} />)
+  .add('extrapolando o wraper', () => <TagsList tags={tags3} />);
