172965e00da67e8b4ff7419be55f76bd809bf38d
Pedro Pinho <rihor1000@gmail.com>
2019-08-26 13:04:50 -0300

    Estilização do TagsList

--- src/components/TagsList/styles.js
+++ src/components/TagsList/styles.js
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style: none;
+  flex-wrap: wrap;
+
+  li {
+    background: #7159c1;
+    border-radius: 12px;
+    padding: 1px 10px;
+    color: rgba(255, 255, 255, 0.8);
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    font-weight: 300;
+    cursor: pointer;
+    margin-bottom: 5px;
+
+    & + li {
+      margin-left: 5px;
+    }
+
+    &:hover {
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+    }
+  }
+`;
