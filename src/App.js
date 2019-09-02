499f26b19c53caff9129b1f821ab3b563c0f6265
Pedro Pinho <rihor1000@gmail.com>
2019-09-02 13:06:12 -0300

    Mudança, BrowserRouter no lugar de Router Talvez eu precisa inserir um gerenciador de historico,  ai vai precisar voltar pro Router eu acho

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@
  */
 
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <GlobalStyle />
       <Routes />
-    </Router>
+    </BrowserRouter>
   );
 }
 
