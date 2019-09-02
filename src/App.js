e47adb6e5fea0d97daa73fcdae7e6beaa2d48962
Pedro Pinho <rihor1000@gmail.com>
2019-09-02 10:23:10 -0300

    Adicionando Routes ao App Routes irá conter todas as rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+/**
+ * Base da aplicação
+ * Composta de rotas e estilização global.
+ */
+
 import React from 'react';
+import { Router } from 'react-router-dom';
+
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-    </>
+      <Routes />
+    </Router>
   );
 }
 
