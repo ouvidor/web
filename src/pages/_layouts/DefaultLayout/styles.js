e8bc63ed24aeb263c3bf23dcf08c5b9fa604bdfd
Pedro Pinho <rihor1000@gmail.com>
2019-09-02 14:07:22 -0300

    Criando layout default Esse componente é um Wrapper, só serve para: Extender a tela toda Colocar um menu no canto esquerdo

---
+++ src/pages/_layouts/DefaultLayout/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  min-height: 100vh;
+  background: linear-gradient(#22202c, #402845);
+`;
+
+export const Menu = styled.div`
+  height: 100%;
+  width: 100px;
+  background: red;
+`;
