60faae27b8ff90f3a07440385548c4fad51413e1
Pedro Pinho <rihor1000@gmail.com>
2019-09-02 13:07:02 -0300

    Add comentários e uma rota de redirecionamento Mudei o nome do Route pra AuthRouter, assim pode usar o Route padrão

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@
  * Componente responsável por agregar todas as rotas da aplicação
  *
  * Switch: permite apenas que uma rota seja acessada
+ * AuthRoute: rota que precisa de autenticação
+ * Route: rota normal, permite a prop render
  */
 
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
 // rota customizada para controle de rotas privadas
-import Route from './Route';
+import AuthRoute from './Route';
 
 // páginas
 import Login from '../pages/Login';
@@ -16,7 +18,9 @@ import Login from '../pages/Login';
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
+      <AuthRoute path="/" exact component={Login} />
+
+      <Route render={() => <Redirect pathname="/" />} />
     </Switch>
   );
 }
