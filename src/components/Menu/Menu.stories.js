2a7149175dd6bbf2b849863dc4043ceb61bf38da
Pedro Pinho <rihor1000@gmail.com>
2019-09-04 12:04:51 -0300

    Add Menu Component Add Menu Stories Fiz com CDD, Componente Driven Devlopment

---
+++ src/components/Menu/Menu.stories.js
@@ -0,0 +1,29 @@
+/**
+ * Testes do componente Menu
+ */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { StaticRouter } from 'react-router-dom';
+
+import Menu from './index';
+import GlobalStyle from '../../styles/global';
+
+storiesOf('MENU|Barra de menu', module)
+  .add('como admin comum', () => (
+    <StaticRouter>
+      <GlobalStyle />
+      <Menu />
+    </StaticRouter>
+  ))
+  .add('como adminMaster', () => (
+    <StaticRouter>
+      <GlobalStyle />
+      <Menu adminMasterStatus />
+    </StaticRouter>
+  ))
+  .add('com botão ativo', () => (
+    <StaticRouter location="/dashboard">
+      <GlobalStyle />
+      <Menu adminMasterStatus />
+    </StaticRouter>
+  ));
