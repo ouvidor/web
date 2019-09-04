c28d71e569098b2bc5884dda31578aa205f5fb27
Pedro Pinho <rihor1000@gmail.com>
2019-09-04 12:07:03 -0300

    Usando o componente Menu Antes estava usando uma estilização apenas

--- src/pages/_layouts/DefaultLayout/styles.js
+++ src/pages/_layouts/DefaultLayout/styles.js
@@ -3,11 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   min-height: 100vh;
-  background: linear-gradient(#22202c, #402845);
-`;
-
-export const Menu = styled.div`
-  height: 100%;
-  width: 100px;
-  background: red;
+  background: linear-gradient(#fff, #000);
+  display: flex;
 `;
