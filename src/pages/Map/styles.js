5605be857277a0d048c8eb4fc259da3341899bcb
Pedro Pinho <rihor1000@gmail.com>
2019-09-05 19:04:33 -0300

    Update na página Map O Stories ficou um pouco zuado

--- src/pages/Map/styles.js
+++ src/pages/Map/styles.js
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
 `;
+
+export const BodyWrapper = styled.div`
+  display: flex;
+  background: linear-gradient(#000, #fff);
+  height: 100%;
+`;
