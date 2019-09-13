c0b84ef72155ec00644ffcd3ef56651d2a397454
Pedro Pinho <rihor1000@gmail.com>
2019-09-13 16:44:42 -0300

    Concertando stories da pág Map

--- src/pages/Map/Map.stories.js
+++ src/pages/Map/Map.stories.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
 
 import Map from './index';
 import GlobalStyle from '../../styles/global';
 
+const Wrapper = styled.div`
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+`;
+
 // TODO stories da página Map não ocupam a página toda
 storiesOf('PAGE|Map', module).add('default', () => (
   <>
-    <GlobalStyle /> <Map />
+    <GlobalStyle />
+    <Wrapper>
+      <Map />
+    </Wrapper>
   </>
 ));
