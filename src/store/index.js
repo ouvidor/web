ec34ffe8daf5f9459d1642c26b1ef9767fd46989
Pedro Pinho <rihor1000@gmail.com>
2019-10-05 14:40:27 -0300

    Criando o rootReducer e utilizando ele na store Iniciando os reducers: auth e admin

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,13 @@
+/**
+ * Cria a Redux Store que guarda a árvore de estado da aplicação.App
+ * A única forma de mudar os dados da Store é chamando o `dispatch()`.
+ * Para específicar diferentes partes da árvore da aplicação criamos diferentes
+ * Reducers e combinamos eles em uma função apenas, utilizando `combineReducers`.
+ */
 import { createStore } from 'redux';
 
-const store = createStore();
+import rootReducer from './modules/rootReducer';
+
+const store = createStore(rootReducer);
 
 export default store;
