64906c937ac3e91c09f515a6518ee46cac73c0a1
Pedro Pinho <rihor1000@gmail.com>
2019-10-06 14:04:22 -0300

    Utilizando o redux-persist e o react-toastify no App.js E aplicando o gerenciador da api do navegador no Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 /**
  * Base da aplicação
- * Composta de rotas e estilização global.
+ * Composta de rotas, estilização global,
+ * notificação global atráves do `react-toastify` e
+ * estado global com o `react-redux`.
  */
 
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-import store from './store';
+
+// react-router-dom ouvirá por mudanças que acontecerem no history
+import history from './services/history';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+          <ToastContainer autoClose={3000} hideProgressBar={false} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
