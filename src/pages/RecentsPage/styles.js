60fa3d0a78254740e54b04e430d16fe8130067df
Pedro Pinho <rihor1000@gmail.com>
2019-10-12 18:29:31 -0300

    Criada base da RecentsPage

--- src/pages/RecentsPage/styles.js
+++ src/pages/RecentsPage/styles.js
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import Pagination from '../../components/Pagination';
+
+export const Background = styled.div`
+  background: #eaedf2;
+  flex: 1;
+`;
+
+export const Container = styled.div`
+  margin: 15px 20px;
+`;
+
+export const ManifestationList = styled.ul`
+  background: rgb(11, 118, 218);
+  padding: 15px 20px;
+  border-radius: 8px;
+
+  > li {
+    background: #fff;
+    border: none;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 15px;
+
+    > span {
+      font-size: 18px;
+      font-weight: bold;
+    }
+  }
+`;
