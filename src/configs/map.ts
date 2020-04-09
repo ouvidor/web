3614eef626b8a7bd2ccfc91608b208e7b3c09cd3
Pedro Pinho <rihor1000@gmail.com>
2020-04-09 13:20:03 -0300

    Adaptando configs para Typescript

--- src/configs/map.ts
+++ src/configs/map.ts
@@ -5,6 +5,6 @@ const googleMapsConfig = {
     longitude: Number(process.env.REACT_APP_GOOGLE_MAPS_LONGITUDE),
     zoom: Number(process.env.REACT_APP_GOOGLE_MAPS_ZOOM),
   },
-};
+}
 
-export default googleMapsConfig;
+export default googleMapsConfig
