57f33e9fb5fdf1f077197226017b772e55420c5f
Pedro Pinho <rihor1000@gmail.com>
2020-04-08 23:52:17 -0300

    ContextAPI adaptado

---
+++ src/store/session/index.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useReducer, useEffect } from "react"
+import sessionReducer from "./reducer"
+import { Action } from "./actions"
+
+import Api from "../../services/api"
+
+export type Provided = {
+  session: IStore
+  dispatch?: React.Dispatch<Action>
+}
+
+const initialState: IStore = {
+  token: undefined,
+  isSigned: false,
+  profile: undefined,
+}
+
+// contêm o provider e o consumer, essencial para o uso do estado global
+export const SessionContext = createContext<Provided>({ session: initialState })
+
+const SessionContextProvider: React.FC = ({ children }) => {
+  const [session, dispatch] = useReducer(sessionReducer, initialState, () => {
+    const localData = localStorage.getItem("session")
+
+    // usa os dados guardados no localStorage
+    if (localData) {
+      const parsedLocalData = JSON.parse(localData)
+      const { token } = parsedLocalData
+      Api.saveToken(token) // coloca o token salvo no axios
+      return parsedLocalData
+    }
+
+    // caso não tenha nada no localStorage
+    return initialState
+  })
+
+  useEffect(() => {
+    localStorage.setItem("session", JSON.stringify(session))
+    // sempre que o token mudar ele é salvo na config do axios
+    Api.saveToken(session.token)
+  }, [session])
+
+  return (
+    <SessionContext.Provider value={{ session, dispatch }}>
+      {children}
+    </SessionContext.Provider>
+  )
+}
+
+export default SessionContextProvider
--- src/store/session/reducer.ts
+++ src/store/session/reducer.ts
@@ -3,22 +3,23 @@
  * O reducer deve ser puro, que significa que não faz chamadas a API, não tem funções não puras,
  * que não faz mutações no estado atual, apenas cria um novo estado.
  */
+import { Action } from "./actions"
 
-export default function auth(state, action) {
+export default function auth(state: IStore, action: Action): IStore {
   switch (action.type) {
-    case 'SIGN_IN':
+    case "SIGN_IN":
       // aplica o token recebido no login, salva o perfil e marca o admin como logado
       return {
         profile: action.payload.profile,
         token: action.payload.token,
         isSigned: true,
-      };
+      }
 
-    case 'SIGN_OUT':
+    case "SIGN_OUT":
       // retira o token, o perfil e marca o admin como deslogado
-      return { token: null, isSigned: false, profile: null };
+      return { token: undefined, isSigned: false, profile: undefined }
 
     default:
-      return state;
+      return state
   }
 }
