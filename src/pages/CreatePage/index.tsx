286531f4baf1c6509a4d050c13eef9b1d572fbcb
MatheusJosef <mjmatheusmj97@gmail.com>
2020-06-03 20:21:19 -0300

    Fix envio de arquivo
    envio de arquivo ao criar manifestação

--- src/pages/CreatePage/index.tsx
+++ src/pages/CreatePage/index.tsx
@@ -93,15 +93,15 @@ const CreatePage: React.FC = () => {
     if (data.files && data.files.length > 0) {
       const formData = new FormData()
 
-      formData.append("manifestation_id", manifestation.id.toString())
       Array.from(data.files).forEach((file) => {
         formData.append("file", file)
       })
 
       const files = await Api.post<IFile[]>({
-        pathUrl: "/files",
+        pathUrl: `/files/manifestation/${manifestation.id}`,
         data: formData,
       })
+
       if (!files || files.length === 0) {
         toast.error("Envio de arquivo falhou inesperadamente")
       }
